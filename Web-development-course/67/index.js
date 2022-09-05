c9ce13196b2583693d1c7913996f8dd0c15b7e81
Mr-Vegit <Mr-Vegit@users.noreply.github.com>
2022-09-05 15:54:18 +0530

    .

--- Web-development-course/67/index.js
+++ Web-development-course/67/index.js
@@ -1,14 +1,18 @@
 const http = require('http');
 const fs = require('fs');
 
-const hostname = '127.0.0.1';//local host
-const port  = 3000;  
+const hostname = '127.0.0.1';
+const port  = 80;  
+const home     = fs.readFileSync('index.html')
+const about    = fs.readFileSync('about.html')
+const services = fs.readFileSync('services.html')
+const contact  = fs.readFileSync('contact.html')
 
 const server = http.createServer((req,res)=>{
     console.log(req.url);
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    res.setHeader('Content-Type', 'text/html');
+    res.end(home);
 })
 
 server.listen(port, hostname, () => {
