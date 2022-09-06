dc39fe20baaf4b08cb0ae0bb1c14a2dfe3f1b782
Mr-Vegit <Mr-Vegit@users.noreply.github.com>
2022-09-06 20:41:44 +0530

    .

---
+++ Web-development-course/69/69.js
@@ -0,0 +1,28 @@
+console.log("This is tutorial 69");
+
+//version meaning and usage
+// meaning of version = a.b.c(ex) here 1 = major update , b = minor update or some new function and c = soem bug patches
+// now lets see with an example
+// here I have launched                                            = 1.0.0 (brand new app)
+// now I want to remove a bug so I introduced a new version        = 1.0.1 (with removed patch)
+// Now I want to introduce a new function in it so the version= 1.1.0 (since there is no bug in this update)
+// Now I want to introduce many functions and remove some too = 1.1.0 (since it is a brand new version or totally modified)
+
+// functions defination
+// npm init                            = to create a node module inside a folder or project
+// npm/node --version                  = to check its version
+// npm install name_module             = to download a module
+// npm install name_module@a.b.c       = to download a specific version of the module a ,b,c refers to version numbers
+// npm install name_module --save-dev  = to download it for the development process
+// npm install name_module --global    = to download it globally so that it can be used from anywhere in the computer
+// npm uninstall name_module           = to delete a module
+// npm view module_name version        = To check the version of the module
+// npm install                         = to install the package manager folder and the modules which were present in it
+// NOTE= we can also use npm i instead of using the full install word for downloading a module 
+
+// NOTE= we can also use ~ for dependencies to make them install always the latest version of the modules and > for making the devDependencies to make them install the latest module and we use ^for make the dependencies and devDependenciesto make them install the acurate module versions
+
+
+
+
+
