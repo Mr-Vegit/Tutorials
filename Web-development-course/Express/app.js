2dc848a8b79dbea3ada3a8f7e64a7613967d5b80
Mr-Vegit <Mr-Vegit@users.noreply.github.com>
2022-09-07 21:15:45 +0530

    .

---
+++ Web-development-course/Express/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app =express();
+const port = 80;//localhost 
+
+
+app.get("/",(req,res)=>{
+  res.status(200).send("This is homepage of my first express app with Vegit");  
+});
+
+app.get("/about",(req,res)=>{
+  res.send("This is about page of my first express app with Vegit");  
+});
+app.get("/this",(req,res)=>{
+  res.status(404).send("this page is not found");  
+});
+
+app.post("/about",(req,res)=>{
+  res.send("This is a post request about page of my first express app with Vegit");  
+});
+
+
+
+app.listen(port,()=>{
+    console.log(`The application has started successfully on port ${port}`);
+});
