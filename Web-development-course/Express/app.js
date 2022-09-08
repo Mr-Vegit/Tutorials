0903a1bbd7b9c7f73e519dd2265b12cc7d6b553f
Mr-Vegit <Mr-Vegit@users.noreply.github.com>
2022-09-08 14:48:43 +0530

    .

--- Web-development-course/Express/app.js
+++ Web-development-course/Express/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require('path');
-const app =express();
+const fs = require('fs');
+const app = express();
 const port = 80;//localhost 
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')); //For serving static files
+app.use(express.urlencoded())
 
 // PUG SPECIFIC STUFF
 app.set('view engine', 'pug'); // set the template engine as pug
@@ -16,6 +18,31 @@ app.get('/',( req, res)=>{
     const params = {'title':'Pubg is a great game',"content": con}
     res.status(200).render('index.pug',params)
 })
+app.post('/',(req,res)=>{
+    user_name = req.body.user_name
+    age = req.body.age
+    gender = req.body.gender
+    address= req.body.address
+    more= req.body.more
+    let User_Data = `
+client          = ${user_name}
+His age         = ${age} 
+His gender is   = ${gender}
+His address is  = ${address}
+More info       = ${more} 
+
+`
+
+    // fs.writeFileSync('output.txt', User_Data) //way showed in the tutorial
+    fs.appendFile('output.txt', User_Data,(req,res)=>{
+        console.log("Your data is saved");
+    })
+
+    const params = {'message':'Your form has been submitted successfully'}
+    res.status(200).render('index.pug',params)
+
+})
+
 
 // START THE SERVER
 app.listen(port,()=>{
