91aa646cbc52a556fd8d17671761036f42f9d26e
Mr-Vegit <85156427+Mr-Vegit@users.noreply.github.com>
2023-01-08 20:12:17 +0530

    .

--- docs/css/nav.js
+++ docs/css/nav.js
@@ -1,36 +1,7 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
 }
-const slider = tns({
-    container : ".my-slider",
-    "slideBy" : 1,
-    "speed" : 400,
-    "nav" : false,
-    loop:false,
-    // controlsContainer : "#controls",
-    prevButton : ".previous",
-    nextButton :".next",
-    mouseDrag : true,
-    responsive: {
-        1600:{
-            items:4,
-            gutter:20
-        },
-        1024:{
-            items:3,
-            gutter:20    
-        },
-        768:{
-            items:2,
-            gutter:20    
-        },
-        480:{
-            items:1
-        }
-    }
-})
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
