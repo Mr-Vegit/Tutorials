49eaaab0c114b49323ae45cfd80af838b2092890
Mr-Vegit <85156427+Mr-Vegit@users.noreply.github.com>
2023-03-07 12:44:40 +0530

    .

--- Web-development-course/MongooseTut/index.js
+++ Web-development-course/MongooseTut/index.js
@@ -8,11 +8,13 @@
 //    console.log("We are connected brother");
 // });
 const mongoose = require("mongoose");
+const ObjectId = require('mongodb').ObjectId;
 
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://localhost:27017/harrykart");
+  mongodb://127.0.0.1:27017
+  await mongoose.connect("mongodb://127.0.0.1:27017");
 
   // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
 }
@@ -20,34 +22,58 @@ var db = mongoose.connection;
 // db.once("open", function () {
 //   console.log("We are connected brother");
 // });
-const kittySchema = new mongoose.Schema({
+// const kittySchema = new mongoose.Schema({
+//   name: String,
+// });
+const UserSchema = new mongoose.Schema({
   name: String,
+  // email: { type: String, required: true, unique: true},
+  email: String,
+  password: String,
+  recent: Array,
+  bookmark: Array
 });
-kittySchema.methods.speak = function () {
-  const greeting = "My name is " + this.name;
-  console.log(greeting);
+// UserSchema.plugin(uniqueValidator)
+const Users = mongoose.model("Users", UserSchema);
+// kittySchema.methods.speak = function () {
+//   const greeting = "My name is " + this.name;
+//   console.log(greeting);
+// };
+const filter = { _id: new ObjectId('63fb75f5ecced2f42732902a') };
+const updateDoc = {
+  // $pull: { $in:{img:"https://gogocdn.net/cover/yugioh-go-rush.png"} },
+  $pull: {
+    recent: {
+      img: 'lorem',
+    }
+  }
 };
+const options = { upsert: true };
+Users.updateMany(filter, updateDoc, options, function (err, res) {
+  if (err) throw err;
+  console.log("1 document updated");
+});
+// const Kitten = mongoose.model("Kitty", kittySchema);
 
-const Kitten = mongoose.model("Kitty", kittySchema);
-
-const HarryKitty = new Kitten({ name: "Eyepatch" });
-const HarryKitty2 = new Kitten({ name: "lord Grim" });
+// const HarryKitty = new Kitten({ name: "Eyepatch" });
+// const HarryKitty2 = new Kitten({ name: "lord Grim" });
 // console.log(HarryKitty.name);
 // HarryKitty.speak();
 
-HarryKitty.save(function (err, HarryKitty) {
-  if (err) return console.error(err);
-  // HarryKitty.speak();
-});
-HarryKitty2.save(function (err, HarryKitty2) {
-  if (err) return console.error(err);
-  // HarryKitty2.speak();
-});
-Kitten.find({ name: "lord Grim" }, function (err, kittens) {
-  if (err) return console.error(err);
-  console.log(kittens);
-});
-Kitten.deleteOne({ name: "Eyepatch" }, function (err, kittens) {
-  if (err) return console.error(err);
-  console.log(kittens);
-});
+// HarryKitty.save(function (err, HarryKitty) {
+//   if (err) return console.error(err);
+//   // HarryKitty.speak();
+// });
+// HarryKitty2.save(function (err, HarryKitty2) {
+//   if (err) return console.error(err);
+//   // HarryKitty2.speak();
+// });
+// Kitten.findOne({ name: "lord Grim" }, function (err, kittens) {
+//   if (err) return console.error(err);
+//   console.log(kittens);
+// });
+// Kitten.findOne({ name: "Eyepatch" }, function (err, kittens) {
+//   if (err) return console.error(err);
+//   console.log(kittens);
+// });
+
